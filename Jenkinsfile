401d8cd8da2f0e838e1ffad8f36693904db95fdb
James Cahill <jcahill773@gmail.com>
2020-05-18 14:13:30 +0100

    Initial PoC using Jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,32 @@
+// Scripted Pipeline
+// Requires libraries from https://github.com/Prouser123/jenkins-tools
+// Made by @Prouser123 for https://ci.jcx.ovh.
+
+// Set ENV variable for ZeroTier version.
+env.ZEROTIER_VERSION = '1.4.6'
+
+node('docker-cli') {
+  cleanWs()
+
+  docker.image('jcxldn/jenkins-containers:base').inside {
+
+    stage('Setup') {
+	  scmVars = checkout scm
+	  
+	  sh 'apk add make gcc build-base linux-headers && \
+		  wget https://github.com/zerotier/ZeroTierOne/archive/${env.ZEROTIER_VERSION}.tar.gz -o zerotier.tar.gz && \
+		  tar zxvf zerotier.tar.gz && \
+		  rm zerotier.tar.gz'
+		  
+	}
+	
+	stage('Build') {
+		sh 'cd ZeroTierOne-${env.ZEROTIER_VERSION}/ && \
+		    sed -i -e "s/-march=armv5//g" make-linux.mk && \
+			make SHARED=0 CC="gcc -static" CXX="g++ -static -march=armv6" && \
+			tar czvf zerotier-one-armv6.tar.gz zerotier-one'
+		
+		archiveArtifacts artifacts: 'ZeroTierOne-${env.ZEROTIER_VERSION}/zerotier-one-armv6.tar.gz', fingerprint: true
+	}
+  }
+}
---
+++ installer.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# ZeroTier One Unofficial Installer Script for Alpine Linux on armv6 (All Raspberry Pi Models)
+
+set -x
+
+mkdir -p /tmp/zt-inst-rpi
+cd /tmp/zt-inst-rpi
+
+wget https://ci.jcx.ovh/job/zerotier-rpi-alpine-builds/lastSuccessfulBuild/artifact/zerotier-one-armv6.tar.gz
+
+tar zxvf zerotier-one-armv6.tar.gz
+
+rm zerotier-one-armv6.tar.gz
+
+# Move exec to sbin
+mv zerotier-one /sbin
+
+# Setup symbolic links
+(cd /sbin; ln -s zerotier-one zerotier-cli; ln -s zerotier-one zerotier-idtool)
+
+# Enable the 'tun' kernel module.
+modprobe tun
+echo tun >> /etc/modules
+
+# Remove the temp files we created
+cd / && rm -r /tmp/zt-inst-rpi
+
+# Create a service to autostart the daemon
+echo -e '#!/sbin/openrc-run
+
+name="Zerotier One Daemon"
+command=/sbin/zerotier-one
+command_args="-d"
+
+depend() {
+	need net
+}
+' > /etc/init.d/zerotier-one
+
+chmod 775 /etc/init.d/zerotier-one
+
+# Have the service start at boot.
+ln -s /etc/init.d/zerotier-one /etc/runlevels/default/zerotier-one
+
+# Add to LBU
+lbu add /sbin/zerotier-cli
+lbu add /sbin/zerotier-idtool
+lbu add /sbin/zerotier-one
+
+lbu add /etc/modules
+
+# Commit the changes to RAM.
+lbu commit -d
